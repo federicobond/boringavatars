0ddc97dcdfc5142259d52115f88a3f286fd88c03
Federico Bond <federicobond@gmail.com>
2023-06-17 23:29:16 -0300

    Replace pytest with standard unittest

--- tests/test_avatars.py
+++ tests/test_avatars.py
@@ -1,10 +1,27 @@
-import pytest
+import unittest
 
 from boringavatars import avatar
 
 
-@pytest.mark.parametrize(
-    "variant", ["beam", "marble", "pixel", "sunset", "bauhaus", "ring"]
-)
-def test_avatar(variant):
-    avatar("foobar", variant=variant)
+class AvatarTests(unittest.TestCase):
+    def test_avatar_beam(self):
+        avatar("foobar", variant="beam")
+
+    def test_avatar_marble(self):
+        avatar("foobar", variant="marble")
+
+    def test_avatar_pixel(self):
+        avatar("foobar", variant="pixel")
+
+    def test_avatar_sunset(self):
+        avatar("foobar", variant="sunset")
+
+    def test_avatar_bauhaus(self):
+        avatar("foobar", variant="bauhaus")
+
+    def test_avatar_ring(self):
+        avatar("foobar", variant="ring")
+
+
+if __name__ == "__main__":
+    unittest.main()
